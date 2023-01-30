14ed0463f344844df3f5a4d7c680fc00e0f266fc
bigb45 <bgd4500@gmail.com>
2023-01-30 15:43:05 +0300

    add style sheet, changed background color and made website nicer-looking

--- script.js
+++ script.js
@@ -9,37 +9,6 @@ function getComputerChoice() {
       return "scissors";
   }
 }
-const buttons = document.querySelectorAll(".buttons>button");
-let playerWins = 0;
-let computerWins = 0;
-buttons.forEach((button) => {
-  console.log(`added event listener for button ${button.id}`);
-  // and for each one we add a 'click' listener
-  button.addEventListener("click", () => {
-    let pc = "rock";
-    let cc = getComputerChoice();
-    // console.log(cc);
-    // pc = prompt("choose rock, paper or scissors!").toLowerCase();
-    // console.log(pc)
-    res = playRound(cc, pc);
-    const results = document.querySelector(".results>.message");
-    const cWins = document.querySelector(".results>.computer")
-    const pWins = document.querySelector(".results>.player")
-    if (res == -1) {
-      results.textContent = `Computer chose ${cc} and won the round`;
-      computerWins += 1;
-    } else if (res == 1) {
-      results.textContent = `Computer chose ${cc} and lost the round`;
-      playerWins += 1;  
-    }
-    else{
-      results.textContent = `Tie Game!`;   
-    }
-    cWins.textContent = `Computer: ${computerWins}`
-    pWins.textContent = `Player: ${playerWins}`
-  });
-});
-
 function playRound(computerChoice, playerChoice) {
   if (computerChoice == playerChoice) {
     console.log(`Tie! ${computerChoice} is the same as ${playerChoice}`);
@@ -57,5 +26,40 @@ function playRound(computerChoice, playerChoice) {
   }
 }
 
-// for (i = 0; i < 5; i++)
-function play() {}
+
+const buttons = document.querySelectorAll(".buttons>button");
+let playerWins = 0;
+let computerWins = 0;
+buttons.forEach((button) => {
+  console.log(`added event listener for button ${button.id}`);
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    let pc = "rock";
+    let cc = getComputerChoice();
+    // console.log(cc);
+    // pc = prompt("choose rock, paper or scissors!").toLowerCase();
+    // console.log(pc)
+    res = playRound(cc, pc);
+    const results = document.querySelector(".message");
+    const cWins = document.querySelector(".results>.computer")
+    const pWins = document.querySelector(".results>.player")
+    if (res == -1) {
+      results.textContent = `Computer wins with ${cc}!`;
+      computerWins += 1;
+    } else if (res == 1) {
+      results.textContent = `Computer loses with ${cc}!`;
+      playerWins += 1;  
+    }
+    else{
+      results.textContent = `Tie Game!`;   
+    }
+    if(playerWins == 5){
+      results.textContent = "Player wins the Game!"
+    }else if(computerWins == 5){
+      results.textContent = "Copmputer wins the Game!"
+    }
+    cWins.textContent = `Computer: ${computerWins}`
+    pWins.textContent = `Player: ${playerWins}`
+  });
+});
+
